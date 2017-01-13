8e17e1393f16320c6e478ca2acb855b96fce8fac
ssheilah <sheilah.schneider@wsu.edu>
2017-01-13 15:48:29 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# WSUWP Radius Pluging
+
+[![Build Status](https://travis-ci.org/ssheilah/wsuwp-radius.svg?branch=master)](https://travis-ci.org/ssheilah/wsuwp-radius)
+
+A plugin to create a shortcode to embed a Radius form using the form's URL.
---
+++ wsuwp-radius.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: WSUWP Radius
+Version: 0.0.1
+Description: A plugin to create a shortcode to embed a Radius form using the form's URL.
+Author: washingtonstateuniversity, ssheilah
+Author URI: https://web.wsu.edu/
+Plugin URI: https://github.com/ssheilah/wsuwp-radius
+Text Domain: [Plugin Text Domain]
+Domain Path: /languages
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// The core plugin class.
+require dirname( __FILE__ ) . '/includes/class-wsuwp-radius.php';
+
+add_action( 'after_setup_theme', 'WSUWP_Radius' );
+/**
+ * Start things up.
+ *
+ * @return \WSUWP_Radius
+ */
+function WSUWP_Radius() {
+	return WSUWP_Radius::get_instance();
+}
